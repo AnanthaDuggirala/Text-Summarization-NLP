b61fc7f959c57e4200d41c2721450bbda85500c9
Anantha Duggirala <ananthaduggirala@Ananthas-MacBook-Pro.local>
2024-05-28 23:02:12 -0400

    data validation code

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -2,7 +2,7 @@
 
 
 from textSummarizer.utils.common import read_yaml, create_driectories
-from textSummarizer.entity import DataIngestionConfig
+from textSummarizer.entity import DataIngestionConfig, DataValidationConfig
 from textSummarizer.constants import *
 
 
@@ -27,3 +27,15 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir,
         )
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_driectories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+        return data_validation_config
